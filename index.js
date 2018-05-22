80dd1b4272655ab6d01f909f5d12e1404c3435e2
durak <itapkos@gmail.com>
2018-05-22 23:37:00 +0300

    h3_06

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+
+    if (body.name === undefined || body.number === undefined) {
+        return response.status(400).json({ error: 'name or number missing' })
+    }
+
+    if (persons.map((p) => p.name).includes(body.name)) {
+        return response.status(400).json({ error: 'name must be unique' })
+    }
+
     const person = {
         name: body.name,
         number: body.number,
